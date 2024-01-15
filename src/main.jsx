6e0ebce0566e0cf7c3eb9dc6c9c5103d8721dd2e
Zukhulento <luis.matus.jr@gmail.com>
2024-01-14 20:26:06 -0600

    updating file names

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { HooksApp } from './HooksApp'
 import "./08-useReducer/intro-reducer"
+import { TodoApp } from './08-useReducer/TodoApp'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HooksApp />
+  <TodoApp />
 )
 
   // <React.StrictMode>
